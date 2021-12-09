3956d9a419bb8c18b6fd5daefc479e74c3875e2d
ROLES Sam <s1869354@ed.ac.uk>
2021-12-09 09:55:15 +0000

    Added comments

--- plotly_paralel_coords2.R
+++ plotly_paralel_coords2.R
@@ -1,32 +1,32 @@
-
-
-
-
-iris<-iris
+# Clear environment
+rm(list=ls()) 
 
 # Load libraries
-rm(list=ls()) # clear environment
-
 library(GGally)
 
 # Read in agricultural data
 agri_data<- read.csv("data/us_agri.csv")
 
-# Plot
-
+# Plot parallel coordinates plot
 (parcoord_plot <- ggparcoord(agri_data,
-           columns = 1:4, groupColumn = 5,
-           scale = "center",
-           title = "Relative Price Per Unit Variable by Farm",
-           alphaLines = 0.6))
+           columns = 1:4, groupColumn = 6, # Variable columns on x axis and which to group lines by
+           scale = "center", # Standardise and center variables
+           title = "Relative Price Per Unit Variable by Farm", # Title
+           alphaLines = 0.6)) # Opacity of lines
 
+# Load libraries
 library(plotly)
-class(us_agri$size)
 library(tidyverse)
-fig <- us_agri %>%
-  plot_ly(type = 'parcoords', line = list(color = ~size,
+
+# Plot  a plotly parallel coordinate plot
+plotly_parcoord <- us_agri %>%
+  plot_ly(type = 'parcoords', line = list(color = ~size_plotly, # Distinguish sizes into three colours
                                           colorscale = list(c(0,'red'),c(0.5,'green'),c(1,'blue'))),
-                                dimensions = list(
+                                dimensions = list( # Define the scale ranges of each variable  
+                        list(range = c(0,1.5),
+                             label = '          Size: Large \r\n       
+                    Medium \r\n
+                 Small', values = ~size_plotly), # A make-shift legend         
                         list(range = c(0,4),
                              label = 'capital', values = ~p.capital),
                         list(range = c(0,4),
@@ -35,8 +35,8 @@ fig <- us_agri %>%
                         list(range = c(0,4),
                              label = 'labor', values = ~p.labor),
                         list(range = c(0,4),
-                             label = 'crop', values = ~p.crop)
-                      )
+                             label = 'crop', values = ~p.crop))
           )
 
-fig
+plotly_parcoord # Call plot
+
